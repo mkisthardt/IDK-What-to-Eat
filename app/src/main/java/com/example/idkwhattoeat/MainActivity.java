e0051a7f93cf9d58f2e6d1d5bf12bf091fbe56d5
[Andrew White] <[awhite@muhlenberg.edu]>
2022-10-18 19:21:20 -0400

    First draft of login

--- app/src/main/java/com/example/idkwhattoeat/MainActivity.java
+++ app/src/main/java/com/example/idkwhattoeat/MainActivity.java
@@ -21,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 }
 
---
+++ app/src/main/java/com/example/idkwhattoeat/SignUpTabFragment.java
@@ -0,0 +1,16 @@
+package com.example.idkwhattoeat;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignUpTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tab_fragment, container, false);
+        return root;
+    }
+}
